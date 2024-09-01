56c7c419711283f498730bcd08a9ccffc6f3cd99
Ömer Yumuşak <67347830+oyumusak@users.noreply.github.com>
2024-09-01 06:08:45 +0300

    Update utils.ml

--- utils.ml
+++ utils.ml
@@ -11,3 +11,17 @@ let findTransition (transitions : transition array) name (read : char) =
 			loop (i + 1)
 	in
 	loop 0
+
+	let findTransitionn (transitions : transition array) name (read : char) =
+		let rec loop i =
+			if i >= Array.length transitions then
+				None
+			else if (transitions.(i).name = name) then
+				if (transitions.(i).read = String.make 1 read) then
+					Some transitions.(i)
+				else
+					loop (i + 1)
+			else
+				loop (i + 1)
+		in
+		loop 0
